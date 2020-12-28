ed3095a455f859a408e1ca95998761afc1be4515
barakg <barakgonen9@gmail.com>
2020-12-28 14:47:53 +0200

    Set up generation for IDL schema conventions

--- src/main/java/org/bg/Main.java
+++ src/main/java/org/bg/Main.java
@@ -1,55 +1,26 @@
-//package org.bg;
-//
-//import org.bg.avro.structures.AnotherUser;
-//import org.bg.avro.structures.User;
-//import org.bg.avro.structures.base.objects.Coordinate;
-//import org.bg.avro.structures.base.objects.NullableTime;
-//import org.bg.avro.structures.base.objects.Time;
-//import org.joda.time.DateTime;
-//
-//public class Main {
-//    public static void main(String[] args){
-//        System.out.println("BGBG");
-//
-//        // Definition of an instance of user which uses base objects schemata
-//        User myUserNullableTime = User.newBuilder()
-//                .setUserCurrentTime(
-//                        Time.newBuilder()
-//                                .setTimeMillis(DateTime.now().getMillis())
-//                                .build())
-//                .setReportingSystemId(12)
-//                .setUserPosition(Coordinate.newBuilder()
-//                        .setAltitude(12345)
-//                        .setLatitude(234555)
-//                        .setLongitude(45666)
-//                        .build())
-//                .build();
-//
-//        // Definition of another user
-//        User myUserWithPredictedTime = User.newBuilder()
-//                .setUserCurrentTime(
-//                        Time.newBuilder()
-//                                .setTimeMillis(DateTime.now().getMillis())
-//                                .build())
-//                .setReportingSystemId(12)
-//                .setUserPosition(Coordinate.newBuilder()
-//                        .setAltitude(12345)
-//                        .setLatitude(234555)
-//                        .setLongitude(45666)
-//                        .build())
-//                .setUserPredictedTime(NullableTime.newBuilder()
-//                        .setTimeMillis(Long.valueOf(23243443))
-//                        .build())
-//                .build();
-//
-//        // Definition of another kind of user generated from different schema, but has the same types.
-//        AnotherUser anotherUser = AnotherUser.newBuilder()
-//                .setReportingSystemId(myUserNullableTime.getReportingSystemId())
-//                .setUserCurrentTime(myUserWithPredictedTime.getUserCurrentTime())
-//                .setUserPosition(myUserNullableTime.getUserPosition())
-//                .setUserPredictedTime(myUserWithPredictedTime.getUserPredictedTime())
-//                .build();
-//
-//        System.out.println(anotherUser);
-//    }
-//}
+package org.bg;
+
+import org.bg.avro.structures.base.objects.Coordinate;
+import org.bg.avro.structures.base.objects.Employee;
+import org.bg.avro.structures.base.objects.NullableTime;
+import org.bg.avro.structures.base.objects.Time;
+import org.bg.avro.structures.objects.Manager;
+import org.joda.time.DateTime;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("BGBG");
+        Manager manager = Manager.newBuilder()
+                .setEmployeeProp(Employee.newBuilder()
+                        .setActive(true)
+                        .setSalary(12323)
+                        .setName("bgbg")
+                        .build())
+                .setHappy(true)
+                .setNullableTime(NullableTime.newBuilder().setTimeMillis(DateTime.now().toDateTime().getMillis()).build())
+                .setPosition(Coordinate.newBuilder().setAltitude(23).setLat(233).setLon(2331).build())
+                .setMustAppearTimeField(Time.newBuilder().setTimeMllis(231111111).build())
+                .build();
+        System.out.println(manager);
+    }
+}
