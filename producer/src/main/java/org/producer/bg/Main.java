e91702c88dc551fa29873a89fa8110f5981112c5
barakg <barakgonen9@gmail.com>
2021-01-06 10:04:33 +0200

    avro producer and docker  compose

--- producer/src/main/java/org/producer/bg/Main.java
+++ producer/src/main/java/org/producer/bg/Main.java
@@ -1,5 +1,8 @@
-package org.bg;
+package org.producer.bg;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.bg.avro.structures.base.objects.Coordinate;
 import org.bg.avro.structures.base.objects.Employee;
 import org.bg.avro.structures.base.objects.NullableTime;
@@ -7,8 +10,10 @@ import org.bg.avro.structures.base.objects.Time;
 import org.bg.avro.structures.objects.Manager;
 import org.joda.time.DateTime;
 
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("BGBG");
         Manager manager = Manager.newBuilder()
                 .setEmployeeProp(Employee.newBuilder()
@@ -55,6 +60,21 @@ public class Main {
 //                    break;
 //            }
 //        });
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
+        props.put("schema.registry.url", "http://192.168.227.132:8081");
+        Producer<String, Manager> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, Manager>("my-bg-topic-avro", Integer.toString(i), manager));
+
+        producer.close();
 //        manager.getSchema().getFields().stream().forEach(field -> {System.out.println(field.name());});
 //        manager.getSchema().getFields().stream().forEach(field -> System.out.println(field.getObjectProps().get("name")));
     }
